44d25ce7b3b6f1a0d7369706a6f438447f0ed306
Jonas <jonasbhjulstad@gmail.com>
2023-11-06 13:38:32 +0100

    SBM_Graph cleanup

--- cmake/external_packages.cmake
+++ cmake/external_packages.cmake
@@ -8,7 +8,11 @@ find_package(QT NAMES Qt6 Qt5 COMPONENTS Core REQUIRED)
 find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Core REQUIRED)
 
 include(cmake/TinyOrm.cmake)
-
+CPMFindPackage(
+    NAME Static_RNG
+    GITHUB_REPOSITORY jonasbhjulstad/Static_RNG
+    GIT_TAG master
+)
 # find_package(casadi REQUIRED HINTS "/home/man/mambaforge/envs/gt/lib/cmake/casadi")
 set(${PROJECT_NAME}_EXTERNAL_PACKAGES ${TINY_ORM_LIBRARIES})
 
